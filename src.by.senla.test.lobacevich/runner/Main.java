19a5dbe40636f059603b2d2bf0fdbf2385df1f4b
Ihar Labatsecich <lobacevich_@tut.by>
2024-07-02 14:23:34 +0300

    add gradle

--- src.by.senla.test.lobacevich/runner/Main.java
+++
@@ -1,7 +0,0 @@
-package runner;
-
-public class Main {
-
-    public static void main(String[] args) {
-    }
-}
---
+++ src/main/java/by/senla/task/lobacevich/runner/Main.java
@@ -0,0 +1,8 @@
+package by.senla.task.lobacevich.runner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
